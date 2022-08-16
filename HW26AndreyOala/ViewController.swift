ec3a674114b1f8b54616ad9a00c17e202ae76159
Andrey <andreyoala@icloud.com>
2022-08-16 12:13:05 +0300

    Initial Commit

---
+++ HW26AndreyOala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW26AndreyOala
+//
+//  Created by Andrey Oala on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
